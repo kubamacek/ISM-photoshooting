5e7906afef1e3f29016f413158a23d72e059814e
kubamacek <kuba.macek@o2.pl>
2020-04-21 20:02:56 +0200

    verify JWT using generated key

--- photoshooting/src/main/java/io/swagger/api/JwtProvider.java
+++ photoshooting/src/main/java/io/swagger/api/JwtProvider.java
@@ -1,5 +1,14 @@
 package io.swagger.api;
 
+import java.io.InputStream;
+import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+import java.security.UnrecoverableKeyException;
+import java.security.cert.CertificateException;
+
 import javax.annotation.PostConstruct;
 import javax.crypto.SecretKey;
 
@@ -12,34 +21,58 @@ import com.fasterxml.classmate.util.ResolvedTypeCache.Key;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.IOException;
 import io.jsonwebtoken.security.Keys;
 
 @Service
 public class JwtProvider {
 
-	private SecretKey key;
+	private KeyStore keyStore;
 	
 	@PostConstruct
-	public void init() {
-		key = Keys.secretKeyFor(SignatureAlgorithm.HS512);
-	}
+    public void init() throws java.io.IOException {
+        try {
+            keyStore = KeyStore.getInstance("JKS");
+            InputStream resourceAsStream = getClass().getResourceAsStream("/photoshooting.jks");
+            keyStore.load(resourceAsStream, "photoshooting".toCharArray());
+        } catch (KeyStoreException | CertificateException | NoSuchAlgorithmException | IOException e) {
+            throw new SpringBlogException("Exception occured while loading keystore");
+        }
+
+    }
 	
 	public String generateToken(Authentication authentication) {
-		User principal = (User) authentication.getPrincipal();
-		return Jwts.builder()
-				.setSubject(principal.getUsername())
-				.signWith(key)
-				.compact();
-	}
-	
+        User principal = (User) authentication.getPrincipal();
+        return Jwts.builder()
+                .setSubject(principal.getUsername())
+                .signWith(getPrivateKey())
+                .compact();
+    }
+
+	private PrivateKey getPrivateKey() {
+        try {
+            return (PrivateKey) keyStore.getKey("photoshooting", "photoshooting".toCharArray());
+        } catch (KeyStoreException | NoSuchAlgorithmException | UnrecoverableKeyException e) {
+            throw new SpringBlogException("Exception occured while retrieving public key from keystore");
+        }
+    }
+
 	public boolean validateToken(String jwt) {
-		Jwts.parser().setSigningKey(key).parseClaimsJws(jwt);
-		return true;
-	}
+        Jwts.parser().setSigningKey(getPublickey()).parseClaimsJws(jwt);
+        return true;
+    }
+
+	private PublicKey getPublickey() {
+        try {
+            return keyStore.getCertificate("photoshooting").getPublicKey();
+        } catch (KeyStoreException e) {
+            throw new SpringBlogException("Exception occured while retrieving public key from keystore");
+        }
+    }
 
 	public String getUsernameFromJwt(String token) {
 		Claims claims = Jwts.parser()
-				.setSigningKey(key)
+				.setSigningKey(getPublickey())
 				.parseClaimsJws(token)
 				.getBody();
 		return claims.getSubject();
---
+++ photoshooting/src/main/java/io/swagger/api/SpringBlogException.java
@@ -0,0 +1,7 @@
+package io.swagger.api;
+
+public class SpringBlogException extends RuntimeException {
+    public SpringBlogException(String message) {
+        super(message);
+    }
+}
